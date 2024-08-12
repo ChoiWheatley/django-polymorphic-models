7070de94e2792e2515d95e976964cdc6c641d538
ChoiWheatley <chltmdgus604@naver.com>
2024-08-12 23:29:01 +0900

    ✨ feat(composite): NULLable한 두 FK를 포함하는 composite 모델을 만들었습니다.

--- composite/models.py
+++ composite/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class RelationA(models.Model):
+    a = models.CharField(max_length=255)
+
+
+class RelationB(models.Model):
+    b = models.CharField(max_length=255)
+
+
+class Composite(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.CharField(max_length=255)
+    relation_a = models.ForeignKey(RelationA, null=True, on_delete=models.CASCADE)
+    relation_b = models.ForeignKey(RelationB, null=True, on_delete=models.CASCADE)
